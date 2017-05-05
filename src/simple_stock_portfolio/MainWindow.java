2332eb6cef0b3999c777f0541985d41aeb46c188
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-05-05 10:57:32 -0400

    first commit, setting up files and classes

---
+++ src/simple_stock_portfolio/MainWindow.java
@@ -0,0 +1,11 @@
+package simple_stock_portfolio;
+
+// Where the main run function for the application will lie
+public class MainWindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
